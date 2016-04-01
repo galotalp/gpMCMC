fb787244aa90c731de86f9c4156927e6e58d171c
Gal Av-Gay <gal.avgay@gmail.com>
2016-03-31 17:17:55 -0700

    bhutyfgh

--- R/testing.R
+++ R/testing.R
@@ -14,20 +14,11 @@ L <- chol(R)
 z <- as.matrix(rnorm(n^2))
 y <- L%*%z
 
+library(MASS)
 
 fitGauss(x,y)
 
-library(MASS)
-init <- rep(0.1, dim(x)[2])
 
-logpost_wrapper <- function(val){
-	d2 <- c(val[1],val[2],0,0) # FIX THIS: MUST generalize to all dimensions
-	cor.par <- data.frame(matrix(data = d2,nrow = dim(x)[2],ncol = 2))
-	names(cor.par) <- c("Theta.y","Alpha.y")
-	return(log_posterior(x, y, f, cor.par, prior = "Exp"))
-}
-
-fit2 <- optim(init,logpost_wrapper, lower = -10, upper = 10)
 
 d <- gpMCMC(100,100,10,x,y)
 mean(d$samples[,1])
